6ce4eb91a4576c4ae744dbb16babd97724bb2733
paulineguermeur <paulineguermeur@ntymail.com>
2026-03-23 17:14:47 +0100

    [FEAT]NON FONCTIONNEL /: controleurCategorie UPDATE de la vue Categorie

--- src/view/Categories.java
+++ src/view/Categories.java
@@ -8,7 +8,9 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import model.Categorie;
+import model.Utilisateur;
 
 /**
  * Servlet implementation class Categories
@@ -29,22 +31,32 @@ public class Categories extends HttpServlet {
 	 */
 	@SuppressWarnings("unchecked")
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		HttpSession h = request.getSession();
+		Utilisateur activeUser = (Utilisateur) h.getAttribute("activeUser");
+		String role = activeUser.getRole();
+		System.out.println("role:" + role);
 		
 		String r = "<!Doctype html><html><head><meta charset=\"utf-8\"/> \r\n\r\n"
 				+ " <link href=\"licence.css\" rel=\"stylesheet\">"
 				+ " </head><body><h1 align=center>Catégories : </h1></br>\r\n"
-				+ "	<div align=center><form action=\"ControleurCategories\" method=GET>\r\n"
+				+ "	<div align=center><form action=\"ControleurCategories\" method=GET>\r\n" //méthode POST => UPDATE en BDD
 				+ " <table border>"
 				+ " <tr><th>Années</th><th>Nom</th></tr>";
 		
 				for(Categorie c : (ArrayList<Categorie>)request.getAttribute("categories")) {
-					r += "<tr><td><input type=\"text\" name=\"annee\" value='"+c.getNomCateg()+"'></td><td><input type=\"text\" name=\"annee\" value='"+c.getIdCateg()+"'></td></tr>";
+					r += "<tr><td><input type=\"text\" name=\"annee\" value='"+c.getNomCateg()+"'></td><td><input type=\"text\" name=\"nom\" value='"+c.getIdCateg()+"'></td></tr>";
 				}
+				
+				if ((activeUser.getRole().equals("admin") || activeUser.getRole().equals("modif"))) {
 				r += "</table>"
-					+ "<input type=\"submit\" name=\"categories\" value=\"Valider les modifications\"></td></tr> </form>"
-					+ "<tr><td><form action=\"CreationCategorie\"> <input type=\"submit\" name=\"creationCategorie\" value=\"Créer une catégorie\"></td></tr></form>"
-					+ "</body></html>";
+					+ "<input type=\"submit\" name=\"modifCategories\" value=\"Valider les modifications\"></td></tr> </form>"
+					+ "<tr><td><form action=\"CreationCategorie\" method=POST> <input type=\"submit\" name=\"creationCategorie\" value=\"Créer une catégorie\"></td></tr></form>"
+					+ "</body></html>";						// POST => INSERT en BDD
+				
+				/*	out.print("<form action=\"ControleurCategories\" name=\"boutonCreerCateg\" method=\"get\"> <input type = \"submit\" name=\"creationCategorie\" value=\"Créer une catégorie\"> </form>");	*/
+			
+				
+				}
 		response.getWriter().append(r);
 	}
 
@@ -57,3 +69,4 @@ public class Categories extends HttpServlet {
 	}
 
 }
+
