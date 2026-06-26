56fd2049bdb5865a694a11348e87ee3cbf67f325
AllanC229 <allan.chvt@gmail.com>
2026-06-26 09:37:07 +0200

    [ADD] Fonction ControleSaisieCritere pour vérifier la valeur des criteres

--- src/tool/ControleDeSaisie.java
+++ src/tool/ControleDeSaisie.java
@@ -32,8 +32,22 @@ public class ControleDeSaisie {
     	    return entreeUtilisateur.contains("<") || entreeUtilisateur.contains(">") || entreeUtilisateur.contains("\"")
     	    	|| entreeUtilisateur.contains("*") || entreeUtilisateur.contains("\'") ||entreeUtilisateur.contains(";");
     	}
-	
-
+    
+    
+	//Cette fonction vérifie si la valeur saisie lors de la modifications de critères est bien un entier entre 1 et 5 (inclus). Le but est d'éviter de saisir des valeurs
+    //aberrantes ou des chaines de caractères vulnérables aux injections.
+    public static boolean ControleSaisieCritere(String valeur) {
+        try {
+            if (Integer.parseInt(valeur) < 0 || Integer.parseInt(valeur) > 5) {          
+            return false;
+        }
+            else {
+            	return true;
+            }
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
 
 	
 	public ControleDeSaisie() {
